a70d8cdbe64cb2220faef0c62b63805190311aa4
shekharr22 <shekharjadhav501@gmail.com>
2024-12-13 20:33:39 +0530

    initial commit

--- 13buildRESTApi/index.js
+++ 13buildRESTApi/index.js
@@ -4,5 +4,25 @@ const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
 
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    `;
+  res.send(html);
+});
 
-app.listen(PORT, () => console.log(`Server Started at`))
+// REST API 
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find(user => user.id === id)
+    return res.json(user)
+});     
+
+app.post("//api/users/:id")
+
+app.listen(PORT, () => console.log(`Server Started at`));
