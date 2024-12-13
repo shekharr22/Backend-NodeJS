8380f50c860e33bb5be98782485035dc6189315e
shekharr22 <shekharjadhav501@gmail.com>
2024-12-13 23:17:49 +0530

    initial commit

--- 13buildRESTApi/index.js
+++ 13buildRESTApi/index.js
@@ -12,17 +12,39 @@ app.get("/users", (req, res) => {
   res.send(html);
 });
 
-// REST API 
+// REST API
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app
+  .get("/api/users/:id", (req, res) => {
     const id = Number(req.params.id);
-    const user = users.find(user => user.id === id)
-    return res.json(user)
-});     
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    //TODO : edit the user with id
+    return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    //TODO : delete the user with id
+    res.json({ status: "Pending" });
+  });
 
-app.post("//api/users/:id")
+// app.post("/api/users", (req, res) => {
+//     // TOOD: Create new user
+//     return res.json({ status: "pending"})
+// });
+
+// app.patch("/api/users/:id", (req, res) => {
+//     // TOOD: Edit the user with id
+//     return res.json({ status: "pending"})
+// });
+
+// app.delete("/api/users/:id", (req, res) => {
+//     // TOOD: Delete the user with id
+//     return res.json({ status: "pending"})
+// });
 
 app.listen(PORT, () => console.log(`Server Started at`));
