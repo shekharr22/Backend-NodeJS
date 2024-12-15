cc0a33d321c1f0d85333e8390c49da17aefdd4ae
shekharr22 <shekharjadhav501@gmail.com>
2024-12-15 19:19:19 +0530

    initial commit

---
+++ 20modelViewControl/routes/user.js
@@ -0,0 +1,50 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+});
+
+router
+  .route("/api/users/:id")
+  .get(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) return req.status(404).json({ error: "User Not found" });
+    return res.json(user);
+  })
+  .patch(async (req, res) => {
+    await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+    return res.json({ status: "Success" });
+  })
+  .delete(async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({ status: "Success" });
+  });
+
+router.post("/api/users", async (req, res) => {
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "ALl fields are required" });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title
+  });
+
+  return res.status(201).json({ msg: "Success" });
+});
+
+module.exports = router;
