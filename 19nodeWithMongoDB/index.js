cd28abd5b96d22739fb40327b4e372db712e232e
shekharr22 <shekharjadhav501@gmail.com>
2024-12-15 18:57:24 +0530

    initial commit

--- 19nodeWithMongoDB/index.js
+++ 19nodeWithMongoDB/index.js
@@ -1,105 +1,106 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
-const fs = require("fs");
-const mongoose = require("mongoose");
-const path = require("path");
-const { type } = require("os");
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 8000;
 
 //Connection
-
-mongoose
-  .connect("mongodb://127.0.0.1:27017/youtube-app-1")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error", err));
+mongoose.connect('mongodb://127.0.0.1:27017/learning')
+.then(()=> console.log("MongoDB Connected"))
+.catch((err) => console.log("Mongo Error" , err));
 
 //Schema
-
 const userSchema = new mongoose.Schema({
-  fistName: {
-    type: String,
-    required: true
-  },
-  lastName: {
-    type: String
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  jobTitle: {
-    type: String
-  },
-  gender: {
-    type: String
-  }
-});
+    firstName: {
+        type: String,
+        required : true,
+    },
+    lastName : {
+        type: String,
+    },
+    email:{
+        type: String,
+        required : true,
+        unique: true,
+    },
+    jobTitle: {
+        type: String,
+    }
+},
+{timestamps: true}
+)
 
-//Model
-const User = mongoose.model("user", userSchema);
+// Model
+const User = mongoose.model('user' , userSchema);
 
-//Middlewares - plugin
-app.use(express.urlencoded({ extended: false }));
+//Middlewares
+app.use(express.urlencoded({extended:false}));
+
+
 
 //Routes
-app.get("/users", (req, res) => {
-  const html = `
+app.get('/users', async(req, res)=>{
+
+    const allDbUsers = await User.find({});
+
+    const html = `
     <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+    ${allDbUsers.map(user => `<li>${user.firstName} - ${user.email}</li>`).join("")}
     <ul>
-    `;
-  res.send(html);
-});
+    `
+    res.send(html)
+})
 
 //REST Api
 
-app.get("/api/users", (req, res) => {
-  console.log(req.headers);
-  return res.json(users);
+app.get('/api/users', async(req, res)=>{
+
+    const allDbUsers = await User .find({});
+    return res.json(allDbUsers);
+
+})
+
+app.
+route('/api/users/:id')
+.get( async(req, res)=>{
+    
+    const user = await User.findById(req.params.id)
+    if(!user) return req.status(404).json({error : 'User Not found'});
+    return res.json(user);
+
+})
+.patch(async (req,res) =>{
+  await User.findByIdAndUpdate(req.params.id ,{ lastName:"Changed" } );
+    return res.json({status : "Success"});
+})
+.delete( async(req , res) => {
+    await User.findByIdAndDelete(req.params.id);
+    return res.json({status : "Success"});
+})
+
+app.post('/api/users' , async(req , res)=>{
+    // TODO : Create new user
+
+    const  body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg: 'ALl fields are required'});
+    }
+
+
+    const result = await User.create({
+        firstName : body.first_name,
+        lastName : body.last_name,
+        email: body.email,
+        gender : body.gender,
+        jobTitle: body.job_title
+    });
+
+    console.log("result" , result);
+
+    return res.status(201).json({msg: "Success"})
 });
 
-app.route("/api/users/:id").get((req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
 
-  // 500 server error
-  // const user = users.find(user => user[0].id === id);
-
-  //If user not found - 404
-  if (!user) return req.status(404).json({ error: "User Not found" });
-  return res.json(user);
-});
-
-app.post("/api/users", async (req, res) => {
-  // TODO : Create new user
-
-  const body = req.body;
-  if (
-    !body ||
-    !body.first_name ||
-    !body.last_name ||
-    !body.email ||
-    !body.gender ||
-    !body.job_title
-  ) {
-    return res.status(400).json({ msg: "ALl fields are required" });
-  }
-
-  const result = await User.create({
-    firstName: body.first_name,
-    lastName: body.last_name,
-    email: body.email,
-    gender: body.gender,
-    jobTitle: body.job_title
-  });
-
-  console.log("result", result);
-  return res.status(201).json({ msg: "success" });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started at Port ${PORT}`);
-});
+app.listen(PORT , ()=>{
+    console.log(`Server started at Port ${PORT}`)
+})
